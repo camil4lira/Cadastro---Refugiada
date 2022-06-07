a8314647cbc178a8b641d244dd08818abb58fcdc
Camila Lira <>
2022-06-07 21:13:11 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function Cadastrar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigada, ' + nome.value + ', os seus dados foram encaminhados com sucesso, damos o prazo de 48 horas para analisarmos e liberarmos o acesso. ');
+    }
+
+}
